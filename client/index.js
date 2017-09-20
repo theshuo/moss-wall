dc9c7de14c7c569cfc3094cdbc07240c2863b50d
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-20 17:29:22 -0500

    Copy/paste boilerplate nonsense

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import Main from './components';
+
+// establishes socket connection
+import './socket';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ server/index.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+const app = express();
+const socketio = require('socket.io');
+module.exports = app;
+
+// if (process.env.NODE_ENV !== 'production') require('../secrets');
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'));
+
+  // body parsing middleware
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use('/api', require('./api'));
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+
+  // sends index.html
+  app.use('', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  });
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+};
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+
+  // set up our socket control center
+  const io = socketio(server);
+  require('./socket')(io);
+};
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  createApp().then(startListening);
+} else {
+  createApp();
+}
