ddf8fe855f00018c6b4c2a3c716e60881cd8a72b
Tim <theshuo@gmail.com>
2017-09-22 14:35:39 -0500

    Adds script to get Meetup category and topic names

---
+++ initMeetupTopics.js
@@ -0,0 +1,44 @@
+require('./secrets');
+const jsonfile = require('jsonfile');
+const rp = require('request-promise');
+const fs = require('fs');
+const path = require('path');
+
+const topicsFile = path.join(__dirname, '/data/topics.json');
+const categoriesFile = path.join(__dirname, '/data/categories.json');
+
+const meetupApiUrl = (endpoint) => {
+  const meetup_root_url = `https://api.meetup.com/`;
+  return `${meetup_root_url}${endpoint}`;
+};
+
+const getEndpoint = (endpoint) => {
+  return {
+    uri: meetupApiUrl(endpoint),
+    qs: {
+      key: process.env.MEETUP_API_KEY,
+      sign: 'true',
+    },
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true,
+  };
+};
+
+let topics;
+if (fs.existsSync(topicsFile)) {
+  console.log(`Reading from ${topicsFile}`);
+  topics = jsonfile.readFileSync(topicsFile);
+} else {
+  rp(getEndpoint('/topics')).then((res) => {
+    const topics = res.results;
+    const topicList = topics.map((topic) => {
+      return topic.name;
+    });
+    jsonfile.writeFile(topicsFile, topicList, function(err) {
+      console.error(err);
+    });
+    console.log(`Wrote topics to ${topicsFile}`);
+  });
+}
