82415c401f6d81c75144bd0ee84674d22e343a4a
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-23 16:21:50 -0500

    Build base styles

--- client/components/main.js
+++ client/components/main.js
@@ -4,11 +4,51 @@ import {connect} from 'react-redux';
 // import {logout} from '../store';
 
 const Main = () => {
-
+  let colors = {
+    background: '#95DBCC',
+    header: '#472E0E',
+  };
+  let styles = {
+    main: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'stretch',
+      height: '100%',
+      background: colors.background,
+      textAlign: 'center',
+    },
+    header: {
+      margin: '4rem 0',
+    },
+    headerText: {
+      fontSize: '4.6rem',
+      fontFamily: '"Galano Grotesque DEMO", "Futura", sans-serif',
+      color: colors.header,
+    },
+    headerHr: {
+      width: '100%',
+      height: '2px',
+      maxWidth: '200px',
+      margin: '1rem auto',
+      border: 'none',
+      borderRadius: '1px',
+      background: colors.header,
+    },
+    content: {
+      flex: 1,
+      fontSize: '1.8rem',
+      fontFamily: '"Nunito", sans-serif',
+    },
+  };
   return (
-    <div id="main">
-      <h1>MAKE UP YER MIND</h1>
-      <hr />
+    <div id="main" style={styles.main}>
+      <header style={styles.header}>
+        <h1 style={styles.headerText}>MAKE UP YER MIND</h1>
+        <hr style={styles.headerHr} />
+      </header>
+      <div id="content" style={styles.content}>
+        <p>what ya gonna do?</p>
+      </div>
     </div>
   );
 };
