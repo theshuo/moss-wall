7b0b1bd93f11cff3a5941fc31267f2fb889e1aa1
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-23 15:11:27 -0500

    Basic front-end structure

--- client/components/main.js
+++ client/components/main.js
@@ -1,38 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {withRouter, Link} from 'react-router-dom';
-import {logout} from '../store';
+// import {logout} from '../store';
 
-/**
- * COMPONENT
- *  The Main component is our 'picture frame' - it displays the navbar and anything
- *  else common to our entire app. The 'picture' inside the frame is the space
- *  rendered out by the component's `children`.
- */
-const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props;
+const Main = () => {
 
   return (
-    <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
+    <div id="main">
+      <h1>MAKE UP YER MIND</h1>
       <hr />
-      {children}
     </div>
   );
 };
@@ -40,29 +16,22 @@ const Main = (props) => {
 /**
  * CONTAINER
  */
-const mapState = (state) => {
-  return {
-    isLoggedIn: !!state.user.id
-  };
-};
+const mapState = () => ({});
+// const mapState = (state) => {
+//   return {
+//   };
+// };
 
-const mapDispatch = (dispatch) => {
-  return {
-    handleClick () {
-      dispatch(logout());
-    }
-  };
-};
+const mapDispatch = () => ({});
+// const mapDispatch = (dispatch) => {
+//   return {
+//   };
+// };
 
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Main));
+export default connect(mapState, mapDispatch)(Main);
 
 /**
  * PROP TYPES
  */
-Main.propTypes = {
-  children: PropTypes.object,
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-};
+// Main.propTypes = {
+// };
