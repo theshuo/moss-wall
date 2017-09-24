1fb851775587174cd3fc2c36312bdde5ec8d77d9
Tim <theshuo@gmail.com>
2017-09-24 18:51:33 -0500

    First draft!

--- client/store/results.js
+++ client/store/results.js
@@ -14,36 +14,23 @@ const emptyResults = {};
 /**
  * ACTION CREATORS
  */
-const loadResults = results => ({type: LOAD_RESULTS, results});
-export const clearResults = () => ({type: CLEAR_RESULTS});
+const loadResults = (results) => ({ type: LOAD_RESULTS, results });
+export const clearResults = () => ({ type: CLEAR_RESULTS });
 
 /**
  * THUNK CREATORS
  */
 
-export const fetchResults = (selections) =>
-  (dispatch) =>
-    dispatch(loadResults({
-      path: [{name: 'wine',  simToPrev: 0}, { name: 'social', simToPrev: 0.238123}, {name: 'Fun Times', simToPrev: 0.8575}],
-      event: {name: 'some event', venue: {
-        country: 'us',
-        localized_country_name: 'USA',
-        city: 'Chicago ',
-        address_1: '127 W Huron Street',
-        name: 'IO Godfrey Rooftop Lounge ',
-        lon: -87.632156,
-        id: 24740306,
-        state: 'IL',
-        lat: 41.894588,
-        repinned: false
-        }},
-    }));
-// axios POST to api/results/
+export const fetchResults = (selections) => (dispatch) =>
+  axios
+    .post('/api/recommendations', selections)
+    .then((res) => dispatch(loadResults(res.data)))
+    .catch((err) => console.error(err));
 
 /**
  * REDUCER
  */
-export default function (state = emptyResults, action) {
+export default function(state = emptyResults, action) {
   switch (action.type) {
     case LOAD_RESULTS:
       return action.results;
