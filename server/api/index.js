a2a466b768dd09d30599eb4df7432868c6c43284
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-24 15:58:57 -0500

    Add api route for getting topic list

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
+const { Topic } = require('../db/models');
 module.exports = router;
 
 router.get('/', (req, res, next) => res.json({ moss: 'wall' }));
 
-router.get('/graph', (req, res, next) => res.json(req.graph));
+router.get('/topics', (req, res, next) => {
+  Topic.findAll({ attributes: [ 'name' ] })
+    .then(topics => res.json(topics.map(topic => topic.name)))
+    .catch(next);
+});
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
