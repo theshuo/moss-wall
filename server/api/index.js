5182c3f317a3517ffde972c9b0c35002277a9bdd
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-24 15:46:30 -0500

    Attach and persist graph on api routes

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,8 @@ module.exports = router;
 
 router.get('/', (req, res, next) => res.json({ moss: 'wall' }));
 
+router.get('/graph', (req, res, next) => res.json(req.graph));
+
 router.use((req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
